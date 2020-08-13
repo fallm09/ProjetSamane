e8f72c0b4c30d2de60686910a3b51545fefdb2fd
Mareme fall <fallmena09@gmail.com>
2020-08-13 09:36:02 +0000

    first commit

---
+++ cache/2f8c2eb45caeefec053f754148855a12043eef67_0.file.testClient.html.php
@@ -0,0 +1,26 @@
+<?php
+/* Smarty version 3.1.30, created on 2020-08-12 17:40:33
+  from "C:\xampp\htdocs\Mesdossiers\projetSamane\samanemvc-master\src\view\client\testClient.html" */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.30',
+  'unifunc' => 'content_5f340d712d3114_82674358',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2f8c2eb45caeefec053f754148855a12043eef67' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\Mesdossiers\\projetSamane\\samanemvc-master\\src\\view\\client\\testClient.html',
+      1 => 1597246831,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5f340d712d3114_82674358 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+yessss<?php }
+}
